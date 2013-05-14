f49f222e68efea6d28780aa1cd45f8f575d88349
ruoshui <851008988@qq.com>
2013-05-14 13:10:52 +0800

    gfgd
    dvvvv

---
+++ yuan/src/cn/wang/yin/ui/zhinan.java
@@ -0,0 +1,13 @@
+package cn.wang.yin.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cn.wang.yin.personal.R;
+
+public class zhinan extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.zhinan);
+	}
+}
