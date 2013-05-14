1fb83bcbd54e3a9af221f4076ca88d0fcfb35561
ruoshui <851008988@qq.com>
2013-05-14 23:18:23 +0800

    加入翻页

--- yuan/src/cn/wang/yin/ui/TabMain.java
+++ yuan/src/cn/wang/yin/ui/TabMain.java
@@ -1,10 +1,16 @@
 package cn.wang.yin.ui;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.AttributeSet;
 import android.view.View;
+import cn.wang.yin.personal.R;
 
 public class TabMain extends FragmentActivity {
 
@@ -12,6 +18,32 @@ public class TabMain extends FragmentActivity {
 	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
+
+		//final ActionBar actionBar = getActionBar();
+		//actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// For each of the sections in the app, add a tab to the action bar.
+		// 注意红色字的地方需要改为你自己的程序名字
+//		actionBar.addTab(actionBar
+//				.newTab()
+//				.setText("版面1号")
+//				.setTabListener(
+//						new TabListener<Location>(this, "tabone",
+//								Location.class)));
+//		actionBar
+//				.addTab(actionBar
+//						.newTab()
+//						.setText("版面2号")
+//						.setTabListener(
+//								new TabListener<express>(this, "tabtwo",
+//										express.class)));
+//		actionBar.addTab(actionBar
+//				.newTab()
+//				.setText("版面3号")
+//				.setTabListener(
+//						new TabListener<CompassActivity>(this, "tabthree",
+//								CompassActivity.class)));
+
 	}
 
 	@Override
@@ -38,9 +70,44 @@ public class TabMain extends FragmentActivity {
 		super.onStart();
 	}
 
-	@Override
 	public View onCreateView(String name, Context context, AttributeSet attrs) {
 		return super.onCreateView(name, context, attrs);
 	}
 
+	public class TabListener<T extends Fragment> implements
+			ActionBar.TabListener {
+		private Fragment mFragment;
+		private final Activity mActivity;
+		private final String mTag;
+		private final Class<T> mClass;
+
+		/**
+		 * Constructor used each time a new tab is created.
+		 * 
+		 * @param activity
+		 *            The host Activity, used to instantiate the fragment
+		 * @param tag
+		 *            The identifier tag for the fragment
+		 * @param clz
+		 *            The fragment's Class, used to instantiate the fragment
+		 */
+		public TabListener(Activity activity, String tag, Class<T> clz) {
+			mActivity = activity;
+			mTag = tag;
+			mClass = clz;
+		}
+
+		/* The following are each of the ActionBar.TabListener callbacks */
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			mFragment = Fragment.instantiate(mActivity, mClass.getName());
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.action_settings, mFragment).commit();
+		}
+
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		}
+
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		}
+	}
 }
