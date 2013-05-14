fcee5582875cbacb4d6d1973fb3a430c1abc8f89
ruoshui <851008988@qq.com>
2013-05-14 12:53:38 +0800

    指南针
    Change-Id: I080645f440aaaee888181018d552540133d665c6
    Signed-off-by: ruoshui <851008988@qq.com>

--- yuan/gen/cn/wang/yin/personal/R.java
+++ yuan/gen/cn/wang/yin/personal/R.java
@@ -46,6 +46,7 @@ containing a value of this type.
     }
     public static final class color {
         public static final int b_font_color=0x7f050009;
+        public static final int background_color=0x7f05003a;
         public static final int black=0x7f050014;
         public static final int certificate_button_grey=0x7f05001b;
         public static final int common_bg_color=0x7f050034;
@@ -70,6 +71,7 @@ containing a value of this type.
         public static final int lightgreen=0x7f05001e;
         public static final int list_line=0x7f050003;
         public static final int main_text_below_yellow=0x7f050018;
+        public static final int marking_color=0x7f05003b;
         public static final int menucolor=0x7f050017;
         public static final int messageinfo=0x7f050015;
         public static final int photo_border1=0x7f050010;
@@ -84,6 +86,7 @@ containing a value of this type.
         public static final int searchresult_button_orange=0x7f05001c;
         public static final int selected=0x7f050038;
         public static final int single_over_trip_first=0x7f050033;
+        public static final int text_color=0x7f05003c;
         public static final int title_back=0x7f050006;
         public static final int trans=0x7f050024;
         public static final int translucent_background=0x7f050025;
@@ -105,59 +108,111 @@ containing a value of this type.
         public static final int yellow_border=0x7f05000d;
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f060006;
+        public static final int compass_margin_top=0x7f060002;
+        public static final int degree_text_margin_top=0x7f060005;
+        public static final int direction_margin_top=0x7f060000;
+        public static final int interval_width=0x7f060001;
+        public static final int text_direction_size=0x7f060003;
+        public static final int text_title_sign_size=0x7f060004;
     }
     public static final class drawable {
-        public static final int arrow=0x7f020000;
-        public static final int bg_splash=0x7f020001;
-        public static final int blank=0x7f020002;
-        public static final int boot_1=0x7f020003;
-        public static final int boot_2=0x7f020004;
-        public static final int boot_3=0x7f020005;
-        public static final int boot_4=0x7f020006;
-        public static final int first_open_btn=0x7f020007;
-        public static final int gplaces=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int page_indicator_focused=0x7f02000a;
-        public static final int page_indicator_unfocused=0x7f02000b;
-        public static final int pop=0x7f02000c;
-        public static final int roll_point0=0x7f02000d;
-        public static final int roll_point1=0x7f02000e;
-        public static final int roll_point2=0x7f02000f;
-        public static final int roll_point3=0x7f020010;
-        public static final int roll_point4=0x7f020011;
-        public static final int splash_anim=0x7f020012;
+        public static final int altitude=0x7f020000;
+        public static final int altitude_bg=0x7f020001;
+        public static final int app_icon=0x7f020002;
+        public static final int arrow=0x7f020003;
+        public static final int background_bottom=0x7f020004;
+        public static final int background_bottom_2=0x7f020005;
+        public static final int background_compass=0x7f020006;
+        public static final int bg_splash=0x7f020007;
+        public static final int blank=0x7f020008;
+        public static final int boot_1=0x7f020009;
+        public static final int boot_2=0x7f02000a;
+        public static final int boot_3=0x7f02000b;
+        public static final int boot_4=0x7f02000c;
+        public static final int calibrate_1=0x7f02000d;
+        public static final int calibrate_2=0x7f02000e;
+        public static final int calibrate_3=0x7f02000f;
+        public static final int calibrate_4=0x7f020010;
+        public static final int calibrate_5=0x7f020011;
+        public static final int calibrate_6=0x7f020012;
+        public static final int calibrate_7=0x7f020013;
+        public static final int calibrate_animation=0x7f020014;
+        public static final int compass=0x7f020015;
+        public static final int degree=0x7f020016;
+        public static final int e=0x7f020017;
+        public static final int e_cn=0x7f020018;
+        public static final int first_open_btn=0x7f020019;
+        public static final int gplaces=0x7f02001a;
+        public static final int guide=0x7f02001b;
+        public static final int ic_launcher=0x7f02001c;
+        public static final int n=0x7f02001d;
+        public static final int n_cn=0x7f02001e;
+        public static final int number_0=0x7f02001f;
+        public static final int number_1=0x7f020020;
+        public static final int number_2=0x7f020021;
+        public static final int number_3=0x7f020022;
+        public static final int number_4=0x7f020023;
+        public static final int number_5=0x7f020024;
+        public static final int number_6=0x7f020025;
+        public static final int number_7=0x7f020026;
+        public static final int number_8=0x7f020027;
+        public static final int number_9=0x7f020028;
+        public static final int page_indicator_focused=0x7f020029;
+        public static final int page_indicator_unfocused=0x7f02002a;
+        public static final int pop=0x7f02002b;
+        public static final int pressure=0x7f02002c;
+        public static final int pressure_bg=0x7f02002d;
+        public static final int roll_point0=0x7f02002e;
+        public static final int roll_point1=0x7f02002f;
+        public static final int roll_point2=0x7f020030;
+        public static final int roll_point3=0x7f020031;
+        public static final int roll_point4=0x7f020032;
+        public static final int s=0x7f020033;
+        public static final int s_cn=0x7f020034;
+        public static final int splash_anim=0x7f020035;
+        public static final int w=0x7f020036;
+        public static final int w_cn=0x7f020037;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0015;
-        public static final int blank_view=0x7f0a0011;
+        public static final int action_settings=0x7f0a0020;
+        public static final int blank_view=0x7f0a001b;
+        public static final int bottom_unit=0x7f0a0013;
         public static final int button1=0x7f0a000a;
+        public static final int compassView=0x7f0a001e;
+        public static final int compass_pointer=0x7f0a0011;
         public static final int editText1=0x7f0a0009;
         public static final int express_list=0x7f0a000b;
         public static final int first_open_imageButton1=0x7f0a0008;
+        public static final int guide_animation=0x7f0a0018;
+        public static final int guide_description=0x7f0a0017;
         public static final int imageView1=0x7f0a000c;
+        public static final int layout_angle=0x7f0a0012;
+        public static final int layout_direction=0x7f0a0010;
         public static final int mybottomviewgroup=0x7f0a0007;
         public static final int myviewpager=0x7f0a0006;
-        public static final int pop_text=0x7f0a000f;
-        public static final int relativeLayout1=0x7f0a0010;
+        public static final int pop_text=0x7f0a0019;
+        public static final int relativeLayout1=0x7f0a001a;
         public static final int scrollView1=0x7f0a0000;
         public static final int seekBar1=0x7f0a0002;
         public static final int seekBar1_textView=0x7f0a0001;
         public static final int seekBar2=0x7f0a0004;
         public static final int seekBar2_textView=0x7f0a0003;
         public static final int sinagle_taavel_map_view=0x7f0a000e;
-        public static final int splash_anim=0x7f0a0012;
-        public static final int splash_pro=0x7f0a0014;
+        public static final int splash_anim=0x7f0a001c;
+        public static final int splash_pro=0x7f0a001f;
         public static final int textView1=0x7f0a0005;
         public static final int textView2=0x7f0a000d;
-        public static final int textView_log=0x7f0a0013;
+        public static final int textView_log=0x7f0a001d;
+        public static final int textview_location_latitude_degree=0x7f0a0014;
+        public static final int textview_location_longitude_degree=0x7f0a0015;
+        public static final int view_compass=0x7f0a000f;
+        public static final int view_guide=0x7f0a0016;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -171,18 +226,51 @@ containing a value of this type.
         public static final int full=0x7f030008;
         public static final int location=0x7f030009;
         public static final int location_title=0x7f03000a;
-        public static final int popview=0x7f03000b;
-        public static final int save=0x7f03000c;
-        public static final int splash_activity=0x7f03000d;
-        public static final int test=0x7f03000e;
+        public static final int main=0x7f03000b;
+        public static final int popview=0x7f03000c;
+        public static final int save=0x7f03000d;
+        public static final int splash_activity=0x7f03000e;
+        public static final int test=0x7f03000f;
+        public static final int zhinan=0x7f030010;
     }
     public static final class menu {
         public static final int main=0x7f090000;
     }
     public static final class string {
         public static final int action_settings=0x7f070001;
+        public static final int altitude=0x7f070017;
+        public static final int altitude_title=0x7f070015;
+        public static final int altitude_unit=0x7f070019;
+        public static final int app_label=0x7f070009;
         public static final int app_name=0x7f070000;
+        public static final int calibrate_failed_no_location=0x7f070012;
+        public static final int calibrate_failed_timeout=0x7f070013;
+        public static final int calibrate_success=0x7f070011;
+        public static final int cannot_get_location=0x7f070008;
+        public static final int dialog_calibrat_altitude=0x7f07001b;
+        public static final int dialog_title_no_network=0x7f07001a;
+        public static final int direction_east=0x7f070020;
+        public static final int direction_north=0x7f07001e;
+        public static final int direction_south=0x7f07001f;
+        public static final int direction_west=0x7f070021;
+        public static final int east=0x7f070005;
+        public static final int getting_location=0x7f07000a;
+        public static final int hello=0x7f070003;
         public static final int hello_world=0x7f070002;
+        public static final int latitude=0x7f07001c;
+        public static final int location_east=0x7f07000d;
+        public static final int location_north=0x7f07000b;
+        public static final int location_south=0x7f07000c;
+        public static final int location_west=0x7f07000e;
+        public static final int longitude=0x7f07001d;
+        public static final int menu_calibrate=0x7f07000f;
+        public static final int menu_calibrate_altitude=0x7f070010;
+        public static final int north=0x7f070004;
+        public static final int pressure=0x7f070016;
+        public static final int pressure_title=0x7f070014;
+        public static final int pressure_unit=0x7f070018;
+        public static final int south=0x7f070006;
+        public static final int west=0x7f070007;
     }
     public static final class style {
         /** 
