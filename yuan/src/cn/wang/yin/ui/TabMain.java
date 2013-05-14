d5e7aa04c81fac9e2c610e70109c4202fadbce5d
ruoshui <851008988@qq.com>
2013-05-14 23:09:12 +0800

    加入FragmentActivity

---
+++ yuan/src/cn/wang/yin/ui/TabMain.java
@@ -0,0 +1,46 @@
+package cn.wang.yin.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TabMain extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	public View onCreateView(String name, Context context, AttributeSet attrs) {
+		return super.onCreateView(name, context, attrs);
+	}
+
+}
